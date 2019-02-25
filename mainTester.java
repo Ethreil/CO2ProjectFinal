2e8bbecd6d2b932d79f09b17d94579b9c23ceda9
Ethreil <37450012+Ethreil@users.noreply.github.com>
2019-02-25 14:05:20 -0600

    Call methods in graphSources to generate JSON Data

--- mainTester.java
+++ mainTester.java
@@ -2,14 +2,20 @@ package com.CarbonProject;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Calendar;
 
 import org.json.JSONException;
 
 public class mainTester {
 	
 	public static void main(String[] args) throws MalformedURLException, IOException, JSONException {
+	
 		
-	graphSources.encodeFutureAtCurrentRate();
+		graphSources.encodePast();
+		graphSources.encodeFutureAtCurrentRate();
+		graphSources.encodeFutureAtRateLastFiveDecades();
+		graphSources.encodeFutureAtMinus5();
+		
 	}
 
 }
